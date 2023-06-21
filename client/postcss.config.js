d15d5084a1c56eb5d1765a910d8f6c9dabc45628
jess-bk <74265740+jess-bk@users.noreply.github.com>
2023-06-21 18:51:12 +0100

    updated postcss to ES module syntax.

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
-const tailwindcss = require("tailwindcss");
-module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [tailwindcss("./tailwind.config.js"), autoprefixer],
 };
