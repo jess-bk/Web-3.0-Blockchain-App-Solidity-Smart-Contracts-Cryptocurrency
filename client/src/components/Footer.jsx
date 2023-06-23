7f8e8a1cc941c4f1550ea4b15c7473df0b1ee510
jess-bk <74265740+jess-bk@users.noreply.github.com>
2023-06-23 09:51:06 +0100

    services and footer page done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,45 @@
+import logo from "../../images1/logo3.png";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} className="w-38 h-10" />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt:-5 w-full">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Market
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Exchange
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Tutorials
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Wallets
+          </p>
+        </div>
+      </div>
+
+      <div className="flex just-center items-center flex-col mt-5">
+        <p className="text-white text-sm text-center">
+          Come join us and hear for the unexpected krypt verse
+        </p>
+        <p className="text-white text-sm text-center font-medium mt-2">
+          info@krypt-verse.com
+        </p>
+      </div>
+
+      <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5" />
+
+      <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+        <p className="text-white text-left text-xs">@kryptverse2023</p>
+        <p className="text-white text-right text-xs">All rights reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
